c86ab17d7014d1f1ea7ad2c1618a78da4a47e080
MLama2059 <lamamanish2059@gmail.com>
2025-11-17 13:46:55 +0545

    Alphanumeric Data Formatting

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+Console.WriteLine("{1} {0}!", first, second); // The order of variables matters
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+Console.WriteLine();
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+Console.WriteLine();
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save: {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+*/
+
+/*
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+yourDiscount += $"A discount of {((price - salePrice) / price):P2}!";
+Console.WriteLine(yourDiscount);
+*/
+
+/*
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"    Shares: {productShares:N3} Product");
+Console.WriteLine($"    Sub Total: {subtotal:C1}");
+Console.WriteLine($"    Tax: {taxPercentage:P2}");
+Console.WriteLine($"    Total Billed: {total:C}");
+*/
+
+
+// string input = "Pad this";
+// // Console.WriteLine(input.PadLeft(12));
+// Console.WriteLine(input.PadLeft(12, '-'));
+// Console.WriteLine(input.PadRight(12, '-'));
+
+/*
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+*/
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},\nAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine();
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.");
+Console.WriteLine();
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.\n");
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += $"{currentReturn:P2}".PadRight(10);
+comparisonMessage += $"{currentProfit:C2}\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += $"{newReturn:P2}".PadRight(10);
+comparisonMessage += $"{newProfit:C2}\n";
+
+
+Console.WriteLine(comparisonMessage);
--- TestProject/obj/Debug/net7.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net7.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb014e183fe9a58032177ab3f039642d385bee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
